a5e8756df6fda9e53e37250fdca8fc55391ea024
mpostma <postma.marin@protonmail.com>
2020-06-22 09:50:11 +0200

    first 2 exercices

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,57 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, prelude::*, Leds, Delay};
+
+
+fn print_state(leds: &mut Leds, states: &[u8; 8]) {
+    for (i, v) in states.iter().enumerate() {
+        match v {
+            0 => leds[i].off(),
+            1 => leds[i].on(),
+            _ => unreachable!(),
+        }
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, Leds) = aux5::init();
+    let ms = 50_u8;
+    loop {
+        for curr in 0..8 {
+            let next = (curr + 1) % 8;
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+            leds[next].on();
+            delay.delay_ms(ms);
+            leds[curr].off();
+            delay.delay_ms(ms);
+        }
+    }
+    let half_period = 100_u16;
+    let mut leds_state = [1, 0 ,0, 0, 0, 0 ,0 ,0];
+    loop {
+        print_state(&mut leds, &leds_state);
+        leds_state = match leds_state {
+            [1, 0 ,0, 0, 0, 0 ,0 ,0] => [1, 1 ,0, 0, 0, 0 ,0 ,0],
+            [1, 1 ,0, 0, 0, 0 ,0 ,0] => [0, 1 ,0, 0, 0, 0 ,0 ,0],
+            [0, 1 ,0, 0, 0, 0 ,0 ,0] => [0, 1 ,1, 0, 0, 0 ,0 ,0],
+            [0, 1 ,1, 0, 0, 0 ,0 ,0] => [0, 0 ,1, 0, 0, 0 ,0 ,0],
+            [0, 0 ,1, 0, 0, 0 ,0 ,0] => [0, 0 ,1, 1, 0, 0 ,0 ,0],
+            [0, 0 ,1, 1, 0, 0 ,0 ,0] => [0, 0 ,0, 1, 0, 0 ,0 ,0],
+            [0, 0 ,0, 1, 0, 0 ,0 ,0] => [0, 0 ,0, 1, 1, 0 ,0 ,0],
+            [0, 0 ,0, 1, 1, 0 ,0 ,0] => [0, 0 ,0, 0, 1, 0 ,0 ,0],
+            [0, 0 ,0, 0, 1, 0 ,0 ,0] => [0, 0 ,0, 0, 1, 1 ,0 ,0],
+            [0, 0 ,0, 0, 1, 1 ,0 ,0] => [0, 0 ,0, 0, 0, 1 ,0 ,0],
+            [0, 0 ,0, 0, 0, 1 ,0 ,0] => [0, 0 ,0, 0, 0, 1 ,1 ,0],
+            [0, 0 ,0, 0, 0, 1 ,1 ,0] => [0, 0 ,0, 0, 0, 0 ,1 ,0],
+            [0, 0 ,0, 0, 0, 0 ,1 ,0] => [0, 0 ,0, 0, 0, 0 ,1 ,1],
+            [0, 0 ,0, 0, 0, 0 ,1 ,1] => [0, 0 ,0, 0, 0, 0 ,0 ,1],
+            [0, 0 ,0, 0, 0, 0 ,0 ,1] => [1, 0 ,0, 0, 0, 0 ,0 ,1],
+            [1, 0 ,0, 0, 0, 0 ,0 ,1] => [1, 0 ,0, 0, 0, 0 ,0 ,0],
+            _ => unreachable!(),
+        };
+        delay.delay_ms(half_period);
+
+    }
 }
--- src/06-hello-world/openocd.gdb
+++ src/06-hello-world/openocd.gdb
@@ -2,5 +2,7 @@ target remote :3333
 set print asm-demangle on
 set print pretty on
 load
+monitor tpiu config internal /tmp/itm.txt uart off 8000000
+monitor itm port 0 on
 break main
 continue
